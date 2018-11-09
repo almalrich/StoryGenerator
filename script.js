200496758db7403aadf12d36e67bdc603017f71a
GregoryCARON <gregory.caron1@gmail.com>
2018-11-09 10:11:54 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var noms[];
+var objets[];
+var temperatures[];
+var lieux[];
+var verbes['coder', 'copier', 'coordonner', 'nager', 'monter', 'avoir', 'être', 'aimer', 'se passionner', 'développer', 'jouer'];
