24cb5af0c338cbcc3122f2375d2f78e530cbd87a
almalrich <seb-bastos16@live.fr>
2018-11-09 10:27:27 +0100

    ajout noms

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var noms[];
+var noms['Michel Varrek','Jim Carrey','Albert Einstein','Vivaldi','Emma Watson','Renaud','Eminen','Paul Rudd','Jacques','Bernadette','Léa','Jhonny depp','The Rock','Billy Obann','Jacquie','Michel','Gotaga''Philippe Etchebest','H.P. Lovecraft', 'Bill Gates'];
 var objets[];
 var temperatures[];
 var lieux[];
