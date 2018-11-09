66dec64a8f0a1ab021078eebbbf0c83ee38c7b8c
GregoryCARON <gregory.caron1@gmail.com>
2018-11-09 11:02:25 +0100

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var noms['Michel Varrek','Jim Carrey','Albert Einstein','Vivaldi','Emma Watson','Renaud','Eminen','Paul Rudd','Jacques','Bernadette','Léa','Jhonny depp','The Rock','Billy Obann','Jacquie','Michel','Gotaga''Philippe Etchebest','H.P. Lovecraft', 'Bill Gates'];
-var objets[];
-var temperatures[];
-var lieux[];
-var verbes['coder', 'copier', 'coordonner', 'nager', 'monter', 'avoir', 'être', 'aimer', 'se passionner', 'développer', 'jouer', 'soulever', 'évoluer', 'voter', 'arnaquer', 'manger', 's\'envoyer', 'enfourner', 'accomplir', 'communiquer'];
+var noms = ['Michel Varrek', 'Jim Carrey', 'Albert Einstein', 'Vivaldi', 'Emma Watson', 'Renaud', 'Eminen', 'Paul Rudd', 'Jacques', 'Bernadette', 'Léa', 'Jhonny depp', 'The Rock', 'Billy Obann', 'Jacquie', 'Michel', 'Gotaga', 'Philippe Etchebest', 'H.P. Lovecraft', 'Bill Gates'];
+var objets = [];
+var temperatures = [];
+var lieux = ['Floyon', 'Wignehies', 'Barcelona', 'Paris', 'Reykjavik', 'Bondy', 'Rio De Janeiro', 'Mons', 'Le Touquet', 'Trélon', 'Fourmies', 'Big Ben', 'Mont Fuji', 'Palais de l\'élysée', 'Mairie de Paris', 'Mc Donald', 'KFC', 'Burger King', 'Moncuq', 'Chutes du Niagara', 'HiPark'];
+var verbes = ['coder', 'copier', 'coordonner', 'nager', 'monter', 'avoir', 'être', 'aimer', 'se passionner', 'développer', 'jouer', 'soulever', 'évoluer', 'voter', 'arnaquer', 'manger', 's\'envoyer', 'enfourner', 'accomplir', 'communiquer'];
